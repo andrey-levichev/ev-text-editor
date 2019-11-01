94e32b7eacbf2c8d7dd9746f23b98386f164c2a7
Andrey Levichev <andrey.levichev@gmail.com>
2019-11-01 14:35:17 -0400

    fixed auto-dependency build, out of source build, parallel build with vc++

--- makefile
+++ makefile
@@ -1,4 +1,5 @@
-COMPILER_FLAGS = -c -std=gnu++14 -Wall -I. -MMD -MT depends.mk -Wno-unused-variable
+COMPILER_FLAGS = -c -std=gnu++14 -Wall -I. -MMD -Wno-unused-variable
+BIN = bin/$(CXX)/$(BUILD)
 
 ifeq ($(BUILD), release)
 COMPILER_FLAGS += -O3 -flto -DDISABLE_ASSERT
@@ -6,6 +7,7 @@ else ifeq ($(BUILD), debug)
 COMPILER_FLAGS += -g
 else
 COMPILER_FLAGS += -Os
+BIN = bin/$(CXX)
 endif
 
 ifeq ($(CXX), g++)
@@ -13,17 +15,20 @@ COMPILER_FLAGS += -Wno-unused-but-set-variable
 LINKER_FLAGS += -lrt
 endif
 
-build: ev
+build: $(BIN) $(BIN)/ev
+
+$(BIN):
+	mkdir -p $(BIN)
 
 rebuild: clean build
 
 clean:
-	-rm ev *.a *.o *.d ev.exe *.lib *.obj *.pdb *.ilk t.* *.log
+	-rm -rf bin t.* *.log
 
-ev: editor.o foundation.o file.o application.o input.o console.o graphics.o main.o
+$(BIN)/ev: $(BIN)/editor.o $(BIN)/foundation.o $(BIN)/file.o $(BIN)/application.o $(BIN)/input.o $(BIN)/console.o $(BIN)/graphics.o $(BIN)/main.o
 	$(CXX) -o $@ $^ $(LINKER_FLAGS)
 
-%.o: %.cpp
-	$(CXX) $(COMPILER_FLAGS) $<
+$(BIN)/%.o: %.cpp
+	$(CXX) -o $@ $< $(COMPILER_FLAGS)
 
--include depends.mk
+-include $(BIN)/*.d
--- nmake.mk
+++ nmake.mk
@@ -1,3 +1,7 @@
+.SUFFIXES: .h
+
+BIN = bin\vcpp\$(BUILD)
+
 !if "$(BUILD)" == "release"
 COMPILER_FLAGS = /O2 /GL
 LINKER_FLAGS = /LTCG
@@ -5,22 +9,23 @@ LINKER_FLAGS = /LTCG
 COMPILER_FLAGS = /Zi
 !else
 COMPILER_FLAGS = /O1
+BIN = bin\vcpp
 !endif
 
-build: ev.exe
+build: $(BIN) $(BIN)\ev.exe
+
+$(BIN):
+	mkdir $(BIN)
 
 rebuild: clean build
 
 clean:
-	-del ev *.a *.o *.d ev.exe *.lib *.obj *.pdb *.ilk t.* *.log
+	-del /s /q $(BIN) t.* *.log
 
-ev.exe: editor.obj foundation.obj file.obj application.obj input.obj console.obj graphics.obj main.obj
+$(BIN)\ev.exe: $(BIN)\editor.obj $(BIN)\foundation.obj $(BIN)\file.obj $(BIN)\application.obj $(BIN)\input.obj $(BIN)\console.obj $(BIN)\graphics.obj $(BIN)\main.obj
 	cl /nologo /MT $** user32.lib ole32.lib dwrite.lib d2d1.lib windowscodecs.lib editor.res /link /out:$@ $(LINKER_FLAGS)
 
-.cpp.obj:
-	cl /c /nologo /std:c++latest /utf-8 /EHsc /MT /W3 /wd4244 /wd4267 /wd4723 /wd4838 /I. \
+.h.cpp:
+.cpp{$(BIN)\}.obj::
+	cl /nologo /c /Fo:$(BIN)\ /MP16 /std:c++latest /utf-8 /EHsc /MT /W3 /wd4244 /wd4267 /wd4723 /wd4838 /I. \
 		/DGUI_MODE /D_UNICODE /DUNICODE /D_CRT_SECURE_NO_WARNINGS /D_CRT_NON_CONFORMING_SWPRINTFS $(COMPILER_FLAGS) $<
-
-!if exists(depends.mk)
-!include depends.mk
-!endif
