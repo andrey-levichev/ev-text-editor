4249f32f123507e4358178a084ee1d9adb80c2d4
Andrey Levichev <andrey.levichev@gmail.com>
2025-11-29 12:45:27 -0500

    added icon to mingw build

--- build_mingw.cmd
+++ build_mingw.cmd
@@ -1,1 +1,2 @@
-g++ -o ev -O2 -DGUI_MODE -I. -municode -mwindows application.cpp console.cpp editor.cpp file.cpp foundation.cpp input.cpp main.cpp graphics.cpp -lole32 -ldwrite -ld2d1 -lwindowscodecs
+windres editor.rc -O coff -o editor.res
+g++ -o ev -O2 -DGUI_MODE -I. -municode -mwindows -static application.cpp console.cpp editor.cpp file.cpp foundation.cpp input.cpp main.cpp graphics.cpp editor.res -lole32 -ldwrite -ld2d1 -lwindowscodecs
